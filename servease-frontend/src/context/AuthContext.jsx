b10f2aeaf98886357ff1638f1da035d935d33522
Prabhash-109 <prabhashjha92560@gmail.com>
2025-11-11 09:37:31 +0530

    change AuthContext.jsx

--- servease-frontend/src/context/AuthContext.jsx
+++ servease-frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useReducer, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://192.168.56.1:5000/api';
+const API_URL = 'http://localhost:5000/api';
 axios.defaults.baseURL = API_URL;
 
 const setAuthToken = token => {
